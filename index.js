05699cd36776d5e4c6e846e8837fca1ca5438058
Upal Barua <upalbarua52@gmail.com>
2022-11-05 10:01:22 +0600

    Added services api

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4w0vbzl.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  const services = client.db('genius-cars').collection('services');
+
+  try {
+    app.get('/services', async (req, res) => {
+      const query = {};
+      const cursor = services.find(query);
+      const data = await cursor.toArray();
+      res.json(data);
+    });
+  } finally {
+  }
+};
+
+run().catch(error => console.error(error));
 
 app.get('/', (req, res) => {
   res.send('Genius Cars server running...');
